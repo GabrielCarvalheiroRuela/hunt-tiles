386762eb13cc8d53377b6efe8a211a6262016600
Gabriel Carvalheiro Ruela <141842543+GabrielCarvalheiroRuela@users.noreply.github.com>
2025-09-03 00:30:54 -0300

    feat: Navegação no menu

--- Assets/Scenes/Menu/MenuController.cs
+++ Assets/Scenes/Menu/MenuController.cs
@@ -1,14 +1,59 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject painelMenu;
+    [SerializeField] private GameObject painelCreditos;
+    [SerializeField] private GameObject painelOpcoes;
+
+    // Botão para iniciar o jogo
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Botão menu opções
+    public void AbrirOptions()
+    {
+        painelMenu.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+    public void FecharOptions()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenu.SetActive(true);
+    }
+
+    // Botão de creditos
+    public void AbrirCreditos(){
+        painelMenu.SetActive(false);
+        painelCreditos.SetActive(true);
+    }
+    public void FecharCreditos()
+    {
+        painelCreditos.SetActive(false);
+        painelMenu.SetActive(true);
+    }
+
+    // Botão "Sair"
+    public void QuitGame(){
+        Debug.Log("Sair do jogo...");
+        Application.Quit();
+
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
